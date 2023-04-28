b48342a1aad174b259cd880cca184cc0aadfa911
Narrop <nb30@hotmail.fr>
2023-04-28 22:48:00 +0200

    Idle player

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -13,34 +13,34 @@ var _is_falling = true
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	
+	sprite.play("Idle")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	else:
-		_is_falling = false
+		sprite.play("Jumping")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		if is_on_floor():
-			sprite.play("Jumping Up")
-		elif _is_falling:
-			sprite.play_backwards("Jumping Up")
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	print(direction)
 	if direction < 0:
+		sprite.play("Walking")
 		sprite.flip_h = true
 	elif direction > 0:
+		sprite.play("Walking")
 		sprite.flip_h = false
 	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 
 
