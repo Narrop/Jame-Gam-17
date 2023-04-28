de61a2133ed8f223a5224706a5339bf40166a289
MarieDNF <66378377+MarieDNF@users.noreply.github.com>
2023-04-28 21:09:33 +0200

    gravity

--- menu/Player.gd
+++ menu/Player.gd
@@ -10,11 +10,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") : #and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
