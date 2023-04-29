afa62ea8523722d3714611eaae4bd1a1b78cb127
MarieDNF <66378377+MarieDNF@users.noreply.github.com>
2023-04-29 13:32:15 +0200

    Menu sound

--- Scenes/menu/Menu.gd
+++ Scenes/menu/Menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Menu_music.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +21,14 @@ func _on_button_crdit_pressed():
 	var error = get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
 	if (error != OK):
 		print('don\'t work');
+
+
+func _on_button_sound_pressed():
+	if ($Button_sound.text in "Sound off"): 
+		$Button_sound.text = "Sound on"
+		$Menu_music.play();
+	elif  ($Button_sound.text in "Sound on"): 	
+		$Button_sound.text = "Sound off"
+		$Menu_music.stop();
+		
+	
