228190aa76dd6af8ea8c0294b86e990a93952159
MarieDNF <66378377+MarieDNF@users.noreply.github.com>
2023-04-29 09:16:49 +0200

    Menu

--- Scenes/menu/Menu.gd
+++ Scenes/menu/Menu.gd
@@ -9,3 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	var error = get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
+	if (error != OK):
+		print('don\'t work');
+
+
+func _on_button_crdit_pressed():
+	var error = get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
+	if (error != OK):
+		print('don\'t work');
