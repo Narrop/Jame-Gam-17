1ab4ff3b5f38f3be74e02a3924fb069333a971b3
Narrop <nb30@hotmail.fr>
2023-04-30 20:06:52 +0200

    Update Room.gd

--- Scenes/Actors/Rooms/Room.gd
+++ Scenes/Actors/Rooms/Room.gd
@@ -11,6 +11,10 @@ func _physics_process(delta):
 		var collision = move_and_collide(velocity * delta)
 		if collision and collision.get_collider().name == "Player":
 			get_tree().reload_current_scene()
+		else:
+			$CollisionShape2D.set_collision_mask_bit(collision_mask, collision.collider.get_collision_layer(), false)
+			collision = move_and_collide(velocity * delta)
+			$CollisionShape2D.set_collision_mask_bit(collision_mask, collision.collider.get_collision_layer(), true)
 
 func _on_player_entered():
 	combat_mode = true
