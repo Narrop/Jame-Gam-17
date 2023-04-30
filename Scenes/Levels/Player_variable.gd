eec43e0bc7386c7211347a81dfcd5ec5abfca8b4
Narrop <nb30@hotmail.fr>
2023-04-30 18:51:17 +0200

    Update Player_variable.gd

--- Scenes/Levels/Player_variable.gd
+++ Scenes/Levels/Player_variable.gd
@@ -3,7 +3,14 @@ extends Node
 var coin = 0
 var score = 0
 var hammers = 0
+var max_score = 0
 
 func reset():
 	score = 0
 	hammers = 0
+	
+	# charge le score maximal à partir du localStorage
+	if Engine.has_singleton("Web"):
+		var local_storage = Engine.get_singleton("Web").local_storage
+		if local_storage.has_feature("localStorage"):
+			max_score = int(local_storage.get_item("max_score"))
