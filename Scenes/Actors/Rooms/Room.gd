15f0890ec24f0d2abb2634cfb02c7acb83d96622
Narrop <nb30@hotmail.fr>
2023-04-30 20:37:47 +0200

    Update Room.gd

--- Scenes/Actors/Rooms/Room.gd
+++ Scenes/Actors/Rooms/Room.gd
@@ -4,6 +4,7 @@ const SPEED = 300
 
 var combat_mode = false
 
+@onready var h_diff = global_position.y - get_parent().get_node("Player").global_position.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -12,8 +13,7 @@ func _physics_process(delta):
 		var collision = move_and_collide(velocity * delta)
 		if collision and collision.get_collider().name == "Player":
 			get_tree().reload_current_scene()
-
-			
+	global_position.y = get_parent().get_node("Player").global_position.y + h_diff
 
 func _on_player_entered():
 	combat_mode = true
