d3527190f17ee2050c492306592fca28103eea5e
Narrop <nb30@hotmail.fr>
2023-04-30 18:51:11 +0200

    Update OnScreenScore.gd

--- Scenes/Levels/OnScreenScore.gd
+++ Scenes/Levels/OnScreenScore.gd
@@ -2,6 +2,22 @@ extends Label
 
 @onready var player_vars = get_node("/root/PlayerVariable")
 
+
+
+func _ready():
+	text = "Score: " + str(player_vars.score) + "\nMax score: " + str(player_vars.max_score)
+
 func _on_timer_timeout():
 	player_vars.score += 1
-	text = "Score: " + str(player_vars.score) # met à jour le texte de l'étiquette avec le score
+	
+	
+	if player_vars.score > player_vars.max_score:
+		player_vars.max_score = player_vars.score
+		# enregistre le score maximal dans le localStorage
+		if Engine.has_singleton("Web"):
+			var local_storage = Engine.get_singleton("Web").local_storage
+			if local_storage.has_feature("localStorage"):
+				local_storage.set_item("max_score", str(player_vars.max_score))
+	
+	# met à jour le texte de l'étiquette avec le score
+	text = "Score: " + str(player_vars.score) + "\nMax score: " + str(player_vars.max_score)
