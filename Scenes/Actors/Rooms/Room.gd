d040419c703d3cdaab38f6485aa01d724dadfe3b
Narrop <nb30@hotmail.fr>
2023-04-30 20:30:58 +0200

    Update Room.gd

--- Scenes/Actors/Rooms/Room.gd
+++ Scenes/Actors/Rooms/Room.gd
@@ -4,6 +4,7 @@ const SPEED = 300
 
 var combat_mode = false
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if not combat_mode:
@@ -11,10 +12,13 @@ func _physics_process(delta):
 		var collision = move_and_collide(velocity * delta)
 		if collision and collision.get_collider().name == "Player":
 			get_tree().reload_current_scene()
-		else:
-			$CollisionShape2D.set_collision_mask_bit(collision_mask, collision.collider.get_collision_layer(), false)
-			collision = move_and_collide(velocity * delta)
-			$CollisionShape2D.set_collision_mask_bit(collision_mask, collision.collider.get_collision_layer(), true)
+
+			
 
 func _on_player_entered():
 	combat_mode = true
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		get_tree().reload_current_scene()
